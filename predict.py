31f03e81cea757eadc8fd86629485a6d1dd55569
kiyotaka-tanaka <tanaka@data-artist.com>
2017-04-19 20:40:00 +0900

    some upload

--- predict.py
+++ predict.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import tensorflow as tf
 import argparse
 import numpy as np
@@ -17,6 +19,7 @@ parser.add_argument("--max_epochs",help="max_epochs",type=int,default=300)
 parser.add_argument("--vocab_size",help = "vocabulary",type=int)
 parser.add_argument("--batch_size",help="batch_size",type =int,default=20)
 parser.add_argument("--vocab_file",help="vocabulary file",type=str,default ="./data_new.txt")
+parser.add_argument("--input",help= "text file to classify",type=str,default="日本の未来")
 
 args = parser.parse_args()
 
@@ -27,4 +30,37 @@ saver = tf.train.Saver()
 
 saver.restore(sess,"./models/my-model.ckpt")
 
+vocab = Vocabulary(args.vocab_file)
 
+vocab.load_vocab()
+
+model.state = model._initial_state
+def input_text(text):
+    
+    text = list(text)
+    
+    out = []
+    
+    for letter in text:
+        out.append(vocab.l2i[letter])
+
+    return out
+
+
+input_text_ = input_text(args.input)
+
+while len(input_text_) < 20:
+    input_text_.append(0)
+input_text_= input_text_[0:20]
+
+input_text_ = np.array(input_text_,dtype=float)
+
+
+
+input_text_ = np.resize(input_text_,(len(input_text_),1))
+
+print input_text_
+
+
+
+print sess.run(model.probs,feed_dict={model.input_data:input_text_,model.real_len:len(input_text_)})
