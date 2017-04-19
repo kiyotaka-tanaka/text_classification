3cf394ee196e985545fda6bddf9146ce7280f2a8
solman <solman@data-artist.com>
2017-04-19 21:20:32 +0900

    save

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--filename_vocab",help = "training_data",type = str,default="./data.txt")
 parser.add_argument("--sequence_length",help = "letter_count",type =int,default=10)
 parser.add_argument("--embedding_size",help= "size of embedding letter to vector",type = int, default=20)
-parser.add_argument("--rnn_size",help="hidden layer size",type=int,default=20)
+parser.add_argument("--rnn_size",help="hidden layer size",type=int,default=21)
 parser.add_argument("--model_path",help="model save path",type=str,default="./models/model.ckpt")
 parser.add_argument("--max_epochs",help="max_epochs",type=int,default=300)
 parser.add_argument("--vocab_size",help = "vocabulary",type=int)
@@ -59,8 +59,11 @@ def train_here():
     #model = TextRnn(args.embedding_size,2,args.rnn_size,args.model_path,args.vocab_size,args.sequence_length,args.batch_size)
 
     model = TextRnn(args.embedding_size,args.vocab_file,2,args.rnn_size)
-
+    
+    saver = tf.train.Saver()
     model.train("data.txt",200)
+    
+    saver.save(model.sess,'my-model.ckpt')
     
     #model.sess.run()
 
